b7d5a19692e485cc82ad90dd40f7a060bf0c3295
imri <imri.paran@gmail.com>
2020-04-15 16:35:16 +0300

    first maya_staeholders dag

---
+++ data-pipeline/dags/maya_stakeholders_local.py
@@ -0,0 +1,48 @@
+from datetime import datetime
+from airflow import DAG
+from airflow.operators.python_operator import PythonOperator
+
+from models.stakeholders import MayaStakeholder
+from operators.json_stream_to_file import handler as json_stream_to_file
+from operators.json_to_sqlite import handler as json_to_sqlite
+
+
+def create_path(path_id, dir_name, source_url, DbModel):
+    create_files = PythonOperator(
+        task_id=f"{path_id}_create_files",
+        python_callable=json_stream_to_file,
+        provide_context=True,
+        op_kwargs={
+            "source_url": source_url,
+            "dest_dir": f"./artifacts/{dir_name}/{{ ts_nodash }}",
+        },
+    )
+
+    insert_to_sqlite = PythonOperator(
+        task_id=f"{path_id}_json_to_sqlit",
+        python_callable=json_to_sqlite,
+        provide_context=True,
+        op_kwargs={
+            "Model": DbModel,
+            "source_dir": f"./artifacts/{dir_name}/{{ ts_nodash }}",
+        },
+    )
+
+    create_files >> insert_to_sqlite
+
+
+with DAG(
+    "maya_stakeholders",
+    catchup=True,
+    start_date=datetime(2019, 1, 1),
+    end_date=datetime(2019, 1, 7),
+    schedule_interval="@daily",
+    max_active_runs=2,
+    concurrency=2,
+) as dag:
+    create_path(
+        path_id="maya_stakeholders",
+        dir_name="stakeholders",
+        DbModel=MayaStakeholder,
+        source_url="https://next.obudget.org/datapackages/maya/maya_company_stakeholder_list/data/maya_stakeholder_list.json",
+    )
