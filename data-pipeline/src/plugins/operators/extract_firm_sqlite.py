d1ebd32b546c72278fed4b6f3d0e2d66ee7b0138
imri <imri.paran@gmail.com>
2020-04-24 15:24:47 +0300

    dont handle errors in process_record

--- data-pipeline/src/plugins/operators/extract_firm_sqlite.py
+++ data-pipeline/src/plugins/operators/extract_firm_sqlite.py
@@ -1,6 +1,4 @@
 from datetime import datetime, timedelta
-from logging import warning
-from typing import List
 from airflow.hooks.sqlite_hook import SqliteHook
 from sqlalchemy.orm import sessionmaker
 from models.sql import WithTimestamps
@@ -9,14 +7,9 @@ from utils.handlers import iteration_handler
 
 
 def process_record(session, execution_date, row):
-    try:
-        Firm.extract_firm(
-            row, created_at=execution_date, updated_at=datetime.utcnow()
-        ).insert_or_update(session)
-    except Exception as err:
-        warning(err)
-        session.rollback()
-        pass
+    Firm.extract_firm(
+        row, created_at=execution_date, updated_at=datetime.utcnow()
+    ).insert_or_update(session)
 
 
 def handler(SourceModel: WithTimestamps, prev_execution_date, execution_date, **kwargs):
