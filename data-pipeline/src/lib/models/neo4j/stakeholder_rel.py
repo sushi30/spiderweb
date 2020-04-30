6e89a28ac45bde6f93d27792ff6868d8396700c9
imri <imri.paran@gmail.com>
2020-04-30 10:19:52 +0300

    changed to datetime format

--- data-pipeline/src/lib/models/neo4j/stakeholder_rel.py
+++ data-pipeline/src/lib/models/neo4j/stakeholder_rel.py
@@ -1,14 +1,16 @@
 from neomodel import (
+    DateTimeFormatProperty,
     FloatProperty,
-    StructuredRel,
-    DateProperty,
     IntegerProperty,
     StringProperty,
+    StructuredRel,
 )
 
 
 class StakeholderRel(StructuredRel):
     capital_percent = FloatProperty()
-    date = DateProperty()
+    date = DateTimeFormatProperty(format="%Y-%m-%d")
     stock_amount = IntegerProperty()
     notes = StringProperty()
+    created_at = DateTimeFormatProperty(format="%Y-%m-%d %H:%M:%S")
+    updated_at = DateTimeFormatProperty(default_now=True, format="%Y-%m-%d %H:%M:%S")
