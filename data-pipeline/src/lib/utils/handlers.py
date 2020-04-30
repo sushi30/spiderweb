58b46f5e04634d927957d22a4f3ea0336cf4032d
imri <imri.paran@gmail.com>
2020-04-30 08:34:06 +0300

    error callback

--- data-pipeline/src/lib/utils/handlers.py
+++ data-pipeline/src/lib/utils/handlers.py
@@ -3,6 +3,11 @@ import traceback
 
 
 def iteration_handler(iterator, handler, error_callback=None):
+    if error_callback is None:
+
+        def error_callback(*args, **kwargs):
+            pass
+
     total = 0
     success = 0
     for item in iterator:
