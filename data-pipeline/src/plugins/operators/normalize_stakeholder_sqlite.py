bd1ccdc0288125bccf78825469bd162ce55d9f27
imri <imri.paran@gmail.com>
2020-04-30 10:54:20 +0300

    optimized imports

--- data-pipeline/src/plugins/operators/normalize_stakeholder_sqlite.py
+++ data-pipeline/src/plugins/operators/normalize_stakeholder_sqlite.py
@@ -1,4 +1,4 @@
-from datetime import datetime, timedelta
+from datetime import datetime
 from airflow.hooks.sqlite_hook import SqliteHook
 from sqlalchemy.orm import sessionmaker
 from models.sql.normalized import Stakeholder
