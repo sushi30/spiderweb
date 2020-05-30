574dc92a3352e2dbb143b6d55a46ee7ea6f33168
imri <imri.paran@gmail.com>
2020-05-30 16:51:33 +0300

    new deployment docker-compose

--- package.sh
+++ package.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 
-EXPORT_NAME="$1-BuildArtifacts"
+INFRA_STACK=$1
+DEPLOYMENT_STACK=$2
+EXPORT_NAME="$INFRA_STACK-BuildArtifacts"
 echo "getting $EXPORT_NAME"
 INFRA_BUCKET=$(python get_export.py $EXPORT_NAME)
 aws cloudformation package --template-file template.yaml --s3-bucket $INFRA_BUCKET --output-template-file packaged.yaml
-aws cloudformation deploy --template-file packaged.yaml --stack-name $2
+aws cloudformation deploy --template-file packaged.yaml --stack-name DEPLOYMENT_STACK
 
-echo "deploying website"
-npm rm -Rf client/.next
-npm run build --prefix client
-echo "deleting old website"
-sls --config client/serverless.yaml
+#echo "deploying website"
+#npm rm -Rf client/.next
+#npm run build --prefix client
+#echo "deleting old website"
+#sls --config client/serverless.yaml
